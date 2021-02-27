25fb861c7f52c4a5adcac8ae933c47c874e767b0
hankyul <consistant1y@ajou.ac.kr>
2021-02-27 12:52:20 +0900

    update action

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,13 +11,24 @@ PRIVATE_REPO="$4"
 PRIVATE_NAME="$5"
 PRIVATE_AUTHOR="$6"
 
-CLONE_DIR=$(mktemp -d)
+PUBLIC_DIR=$(mktemp -d)
 echo "Cloning public git repository"
 git config --global user.email "$PUBLIC_AUTHOR"
 git config --global user.name "$PUBLIC_NAME"
-git clone --single-branch --branch main "https://$API_TOKEN_GITHUB@github.com/$PUBLIC_NAME/$PUBLIC_REPO.git" "$CLONE_DIR"
+git clone --single-branch --branch main "https://$API_TOKEN_GITHUB@github.com/$PUBLIC_NAME/$PUBLIC_REPO.git" "$PUBLIC_DIR"
 
-ls -la "$CLONE_DIR"
+PRIVATE_DIR=$(mktemp -d)
+echo "Cloning private git repository"
+git clone --single-branch --branch main "https://$API_TOKEN_GITHUB@github.com/$PRIVATE_NAME/$PRIVATE_REPO.git" "$PRIVATE_REPO"
+
+cp -r "$PUBLIC_DIR"/submission/* "$PRIVATE_DIR"/submission/
+cp "$PUBLIC_DIR"/README.md "$PRIVATE_DIR"/README.md
+cd "$PRIVATE_DIR"/
+python ReaderBoard/main.py
+cd ..
+cp -r "$PRIVATE_DIR"/submission/* "$PUBLIC_DIR"/submission/
+cp -r "$PRIVATE_DIR"/Log/* "$PUBLIC_DIR"/Log/
+cp "$PRIVATE_DIR"/README.md "$PUBLIC_DIR"/README.md
 
 time=$(date)
 if true ; then
