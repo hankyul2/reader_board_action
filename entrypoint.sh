926b08a606b8aded3aeb10f3fdf2628fc0c7e47d
hankyul <consistant1y@ajou.ac.kr>
2021-02-27 12:14:10 +0900

    FIX bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,14 @@ set -u  # script fails if trying to access to an undefined variable
 
 echo "Starts"
 echo "$INPUT_PUBLIC_AUTHOR"
-echo "$INPUT_PUBLIC_PUBLIC_NAME"
+echo "$INPUT_PUBLIC_NAME"
 echo "$GITHUB_REPOSITORY"
 
 CLONE_DIR=$(mktemp -d)
 echo "Cloning public git repository"
 git config --global user.email "$INPUT_PUBLIC_AUTHOR"
 git config --global user.name "$INPUT_PUBLIC_NAME"
-git clone --single-branch --branch main "https://github.com/$GITHUB_REPOSITORY.git" "$CLONE_DIR"
+git clone --single-branch --branch main "https://$API_TOKEN_GITHUB@github.com/$GITHUB_REPOSITORY.git" "$CLONE_DIR"
 
 ls -la "$CLONE_DIR"
 
